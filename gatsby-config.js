318a22bc5640cfc3a430c731c486b5ac054d511e
mendong <mendong@users.noreply.github.com>
2023-10-17 19:51:44 +0900

    ignore public

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,16 +9,12 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Mendong`,
+      summary: `developer`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    description: `blog`,
   },
   plugins: [
     `gatsby-plugin-image`,
